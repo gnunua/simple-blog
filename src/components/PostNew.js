d8e777c683a9fdae19db162532745cee57081158
gnun <gnunua@gmail.com>
2017-05-25 19:21:14 +0400

    improved create post form

--- src/components/PostNew.js
+++ src/components/PostNew.js
@@ -5,35 +5,73 @@ import {connect} from "react-redux";
 import {createPost} from "../actions";
 
 let PostNew = (props) => {
-
     const submitHandler = (fromData) => {
         props.dispatch(createPost(fromData));
     };
 
     return (
         <div>
-             <form onSubmit={props.handleSubmit(submitHandler)}>
-                <div>
-                    <label htmlFor="title">Title</label>
-                    <Field name="title" component="input" type="text"/>
-                </div>
-                <div>
-                    <label htmlFor="category">Category</label>
-                    <Field name="category" component="input" type="text"/>
-                </div>
-                <div>
-                    <label htmlFor="content">Content</label>
-                    <Field name="content" component="textarea" type="text"/>
-                </div>
-                <button type="submit">Submit</button>
+            <form onSubmit={props.handleSubmit(submitHandler)}>
+                <Field
+                    name="title"
+                    type="text"
+                    component={renderField}
+                    label="title"
+                />
+
+                <Field
+                    name="category"
+                    type="text"
+                    component={renderField}
+                    label="category"
+                />
+
+                <Field
+                    name="content"
+                    type="textarea"
+                    component={renderField}
+                    label="content"
+                />
+
+                <button type="submit" disabled={props.submitting || props.pristine}>Submit</button>
+
                 <Link to="/">Cancel</Link>
             </form>
         </div>
     );
 };
 
+const renderField = ({input, label, type, meta: {touched, error, warning}}) => (
+    <div>
+        <label>{label}</label>
+        <div>
+            <input {...input} placeholder={label} type={type}/>
+            {touched &&
+            ((error && <span>{error}</span>) ||
+            (warning && <span>{warning}</span>))}
+        </div>
+    </div>
+);
+
+const validate = (values) => {
+    const errors = {};
+    if (!values.title) {
+        errors.title = '*required';
+    }
+
+    if (!values.category) {
+        errors.category = '*required';
+    }
+
+    if (!values.content) {
+        errors.content = '*required';
+    }
+    return errors;
+};
+
 PostNew = reduxForm({
-    form: 'createPost'
+    form: 'createPost',
+    validate
 })(connect()(PostNew));
 
 export default PostNew;
