f18f4246923fd609017abd247a37a3f3264df767
gnun <gnunua@gmail.com>
2017-05-25 12:38:41 +0400

    configuration for roting

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,8 @@ module.exports = {
 
     output: {
         path: __dirname + '/dist',
-        filename: "index_bundle.js"
+        filename: "index_bundle.js",
+        publicPath: '/'
     },
 
     module: {
