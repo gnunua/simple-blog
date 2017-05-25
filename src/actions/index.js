06de7de72acb034624c1a9c23ceffa4570baea9f
gnun <gnunua@gmail.com>
2017-05-26 02:15:00 +0400

    created backend api interface

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 import * as Actions from "./actionTypes";
-import {API_BASE_URL, KEY} from "../consts";
-import {makeUtlQueryString} from "../helpers";
+import {createRequest, handleRequest} from "../helpers";
 
 const fetchPostListStart = () => ({
     type: Actions.FETCH_POSTS_LIST_START
@@ -16,28 +15,12 @@ const fetchPostListFail = (err) => ({
     payload: err
 });
 
-export const fetchPostList = () => {
-    const request = fetch(`${API_BASE_URL}/posts?${makeUtlQueryString({key: "gnunuaapikey"})}`);
-
-    return function (dispatch) {
-
-        dispatch(fetchPostListStart());
-
-        request
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (json) {
-                dispatch(fetchPotListSuccess(json));
-                console.log('parsed json', json);
-            })
-            .catch(function (ex) {
-                dispatch(fetchPostListFail(ex));
-                console.log('parsing failed', ex);
-            });
-
-    };
-};
+export const fetchPostList = () => handleRequest(
+    createRequest('GET', '/posts'),
+    fetchPostListStart,
+    fetchPotListSuccess,
+    fetchPostListFail
+);
 
 const fetchPostStart = () => ({
     type: Actions.FETCH_POST_START
@@ -53,30 +36,12 @@ const fetchPostFail = (err) => ({
     payload: err
 });
 
-export const fetchPost = (id) => {
-
-    const request = fetch(`${API_BASE_URL}/posts/${id}?${makeUtlQueryString({key: "gnunuaapikey"})}`);
-
-    return function (dispatch) {
-
-        dispatch(fetchPostStart());
-
-        request
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (json) {
-                dispatch(fetchPostSuccess(json));
-                console.log('parsed json', json);
-            })
-            .catch(function (ex) {
-                dispatch(fetchPostFail(ex));
-                console.log('parsing failed', ex);
-            });
-
-    };
-
-};
+export const fetchPost = (id) => handleRequest(
+    createRequest('GET', `/posts/${id}`),
+    fetchPostStart,
+    fetchPostSuccess,
+    fetchPostFail
+);
 
 const deletePostStart = () => ({
     type: Actions.DELETE_POST_START
@@ -92,31 +57,12 @@ const deletePostFail = (err) => ({
     payload: err
 });
 
-export const deletePost = (id) => {
-    const request = fetch(`${API_BASE_URL}/posts/${id}?${makeUtlQueryString({key: "gnunuaapikey"})}`,
-        {method: 'DELETE'}
-    );
-
-    return function (dispatch) {
-
-        dispatch(deletePostStart());
-
-        request
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (json) {
-                dispatch(deletePostSuccess(json));
-                console.log('parsed json', json);
-            })
-            .catch(function (ex) {
-                dispatch(deletePostFail(ex));
-                console.log('parsing failed', ex);
-            });
-
-    };
-
-};
+export const deletePost = (id) => handleRequest(
+    createRequest('DELETE', `/posts/${id}`),
+    deletePostStart,
+    deletePostSuccess,
+    deletePostFail
+);
 
 const createPostStart = () => ({
     type: Actions.CREATE_POST_START
@@ -132,36 +78,9 @@ const createPostFail = (err) => ({
     payload: err
 });
 
-export const createPost = (payload) => {
-
-    const request = fetch(`${API_BASE_URL}/posts/?${makeUtlQueryString({key: "gnunuaapikey"})}`,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-
-            body: JSON.stringify(payload)
-        }
-    );
-
-    return function (dispatch) {
-
-        dispatch(createPostStart());
-
-        request
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (json) {
-                dispatch(createPostSuccess(json));
-                console.log('created json', json);
-            })
-            .catch(function (ex) {
-                dispatch(createPostFail(ex));
-                console.log('created failed', ex);
-            });
-
-    };
-
-};
+export const createPost = (payload) => handleRequest(
+    createRequest('POST', '/posts', undefined, payload),
+    createPostStart,
+    createPostSuccess,
+    createPostFail
+);
--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,5 @@
+import {API_BASE_URL, KEY} from "./consts";
+
 export const asyncStatus = (isStarted = false, isLoaded = false, isFailed = false, error = null) => ({
     isStarted,
     isLoaded,
@@ -10,4 +12,40 @@ export const makeUtlQueryString = (params) => {
     return Object.keys(params)
         .map(k => esc(k) + '=' + esc(params[k]))
         .join('&');
+};
+
+export const handleRequest = (request, start, success, fail) => dispatch => {
+    dispatch(start());
+    request
+        .then(checkStatus)
+        .then(parseJSON)
+        .then(response => {
+            dispatch(success(response));
+        })
+        .catch(err => {
+            console.error(err);
+            dispatch(fail(err));
+        });
+};
+
+export const createRequest = (variable, url, params = {key: "gnunuaapikey"}, payload) => {
+    const fetchParams = {method: variable};
+    if (variable === 'POST') {
+        fetchParams.body = JSON.stringify(payload);
+        fetchParams.headers = {
+            'Content-Type': 'application/json'
+        };
+    }
+    return fetch(`${API_BASE_URL}${url}?${makeUtlQueryString(params)}`, fetchParams);
+};
+
+const parseJSON = (response) => response.json();
+
+const checkStatus = (response) => {
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    }
+    let error = new Error(response.statusText);
+    error.response = response;
+    throw error;
 };
