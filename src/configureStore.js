cb0b5bb5131d3a99e243a43e8f08674e24e1f38c
gnun <gnunua@gmail.com>
2017-08-23 00:30:59 +0400

    first usage of flow-typed

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,15 +1,16 @@
 // @flow
 
 import {applyMiddleware, createStore, compose} from "redux";
+import type { Store, StoreCreator } from 'redux';
+
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "./reducers";
 
-const configureStore = ():void => {
+const configureStore = ():Store<*, *> => {
 
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
     return createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
-
 };
 
 export default configureStore;
